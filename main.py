73acd6942a59f2ab757161f19787a7b76785ff2e
NelsonH <nelson.hoang@gmail.com>
2025-06-01 22:27:29 -0700

    Resize Image

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ cap = cv2.VideoCapture('Videos/vid (1).mp4')
 
 while True:
     success, img = cap.read()
+    img = cv2.resize(src=img, dsize=(640, 480), dst=None, fx=0.7, fy=0.7)
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cv2.waitKey(50)
